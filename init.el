e98b0b84b23e4fa2acb7d0720ba07ccf348e44ff
Augusto Coronel <aocoronel@proton.me>
2026-04-26 15:52:13 -0300

    chore: move minimal emacs configs to external file

--- init.el
+++ init.el
@@ -1,125 +1,16 @@
 ;; -*- lexical-binding: t; -*-
-;; (desktop-save) ;; sessions
-;; (eww) ;; browser
 
 ;; Some useful configs from: https://github.com/jamescherti/minimal-emacs.d
-;; BEGIN: minimal-emacs.d
-(setq native-comp-async-query-on-exit t)
+(load-file "~/.emacs.d/minimal-emacs.el")
 
-(setq read-answer-short t)
-(if (boundp 'use-short-answers)
-    (setq use-short-answers t)
-  (advice-add 'yes-or-no-p :override #'y-or-n-p))
+(setq completions-detailed t)
+(setq ffap-machine-p-known 'reject)
 
-(setq undo-limit (* 13 160000)
-      undo-strong-limit (* 13 240000)
-      undo-outer-limit (* 13 24000000))
+(setq-default bidi-display-reordering 'left-to-right
+              bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)
 
-(setq truncate-string-ellipsis "…")
-
-(setq eval-expression-print-length nil
-      eval-expression-print-level nil)
-
-(setq x-underline-at-descent-line t)
-
-;; (setq remote-file-name-inhibit-cache 50)
-;;
-;; (setq imenu-auto-rescan t)
-;; (setq imenu-max-item-length 160)
-;; (setq next-line-add-newlines nil)
-;; (setq bookmark-save-flag 1)
-;;
-;; (setq uniquify-buffer-name-style 'forward)
-
-(setq create-lockfiles nil)
-(setq make-backup-files t)
-
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name "backup" user-emacs-directory))))
-(setq tramp-backup-directory-alist backup-directory-alist)
-(setq backup-by-copying-when-linked t)
-(setq backup-by-copying t)
-(setq delete-old-versions t)
-(setq version-control t)
-(setq kept-new-versions 5)
-(setq kept-old-versions 5)
-
-(setq auto-save-include-big-deletions t)
-
-(setq auto-save-list-file-prefix
-      (expand-file-name "autosave/" user-emacs-directory))
-(setq tramp-auto-save-directory
-      (expand-file-name "tramp-autosave/" user-emacs-directory))
-
-(setq auto-save-no-message t)
-(setq auto-save-default t)
-(setq auto-save-interval 300)
-(setq auto-save-timeout 30)
-
-(setq vc-git-print-log-follow t)
-(setq vc-make-backup-files nil)
-(setq kept-old-versions 10)
-(setq kept-new-versions 10)
-(setq vc-git-diff-switches '("--histogram"))
-
-(setq kill-buffer-delete-auto-save-files t)
-(setq kill-do-not-save-duplicates t)
-
-(setq revert-without-query (list ".")  ; Do not prompt
-      auto-revert-stop-on-user-input nil
-      auto-revert-verbose t)
-
-(setq global-auto-revert-non-file-buffers t)
-(setq global-auto-revert-ignore-modes '(Buffer-menu-mode))
-
-(setq auto-revert-interval 3)
-(setq auto-revert-remote-files nil)
-(setq auto-revert-use-notify t)
-(setq auto-revert-avoid-polling nil)
-(setq auto-revert-verbose t)
-(add-hook 'emacs-startup-hook #'global-auto-revert-mode)
-
-(setq save-place-file (expand-file-name "saveplace" user-emacs-directory))
-(setq save-place-limit 600)
-(setq savehist-additional-variables
-  '(kill-ring                        ; clipboard
-    register-alist                   ; macros
-    mark-ring global-mark-ring       ; marks
-    search-ring regexp-search-ring))
-
-(setq history-length 300)
-(setq savehist-save-minibuffer-history t)  ;; Default
-(setq savehist-additional-variables
-      '(kill-ring                        ; clipboard
-        register-alist                   ; macros
-        mark-ring global-mark-ring       ; marks
-        search-ring regexp-search-ring)) ; searches
-
-(setq window-resize-pixelwise nil)
-
-(setq window-divider-default-bottom-width 1
-      window-divider-default-places t
-      window-divider-default-right-width 1)
-
-(setq redisplay-skip-fontification-on-input t)
-
-(setq fast-but-imprecise-scrolling t)
-
-(setq auto-window-vscroll nil)
-
-(setq-default cursor-in-non-selected-windows nil)
-(setq highlight-nonselected-windows nil)
-
-(setq global-text-scale-adjust-resizes-frames nil)
-
-(setq comment-multi-line t)
-(setq comment-empty-lines t)
-
-
-(setq sentence-end-double-space nil)
-(setq lazy-highlight-initial-delay 0)
-;; END: minimal-emacs.d
-
+(blink-cursor-mode 0)
 (which-key-mode 1)
 (setq which-key-separator "  ")
 (setq which-key-prefix-prefix "... ")
@@ -218,7 +109,6 @@
     (eshell t)))
 
 (defun backward-mark-word (arg)
-   "Similar to M-d, but backwards"
    (interactive "p")
    (unless (eq last-command this-command)
     (set-mark (point)))
@@ -377,6 +267,50 @@ This command does the inverse of `fill-paragraph'."
 
 (rc/require 'vterm)
 
+;; === Eglot ===
+
+(rc/require 'eglot)
+(setq eglot-autoshutdown t
+      eglot-send-changes-idle-time 0.5
+      eglot-sync-connect 0
+      eglot-sync-connect nil)
+(setq eglot-events-buffer-size 0)
+(setq jsonrpc-default-request-timeout 5)
+(setq eglot-report-progress nil)
+
+(setq jsonrpc-event-hook nil)
+(setq eglot-events-buffer-config '(:size 0 :format short))
+
+(add-hook 'c-mode-hook #'eglot-ensure)
+
+(setq eglot-ignored-server-capabilities
+      '(
+;;         :hoverProvider
+;;         :completionProvider
+;;         :signatureHelpProvider
+;;         :definitionProvider
+;;         :typeDefinitionProvider
+;;         :implementationProvider
+;;         :declarationProvider
+;;         :referencesProvider
+         :documentHighlightProvider
+;;         :documentSymbolProvider
+;;         :workspaceSymbolProvider
+         :codeActionProvider
+         :codeLensProvider
+         :documentFormattingProvider
+         :documentRangeFormattingProvider
+         :documentOnTypeFormattingProvider
+         :renameProvider
+         :documentLinkProvider
+         :colorProvider
+         :foldingRangeProvider
+         :executeCommandProvider
+         :inlayHintProvider
+         ))
+
+(rc/require 'command-log-mode)
+
 ;; === GNU Global ===
 
 (rc/require 'ggtags)
@@ -412,7 +346,7 @@ This command does the inverse of `fill-paragraph'."
 ;; === Programming Major Modes ===
 
 ;; c-mode
-(setq-default c-basic-offset 8
+(setq-default c-basic-offset 4
               c-default-style '((java-mode . "java")
                                 (awk-mode . "awk")
                                 (other . "bsd")))
@@ -456,14 +390,7 @@ This command does the inverse of `fill-paragraph'."
   ((eq system-type 'windows-nt) (rc/require 'cl-lib)))
 
 (rc/require 'magit)
-;;(rc/require 'forge)
-
 (setq magit-auto-revert-mode nil)
-
-(rc/require 'magit-todos)
-(magit-todos-mode 1)
-(setq magit-todos-keywords
-      '("TODO" "FIX" "BUG" "HACK" "NOTE"))
 
 ;; === Multiple Cursors ===
 
@@ -516,11 +443,6 @@ This command does the inverse of `fill-paragraph'."
 ;; === Move Text ===
 
 (rc/require 'move-text)
-
-;; === Projectile ===
-
-(rc/require 'rg)
-(rc/require 'projectile)
 
 ;; === Company ===
 
@@ -577,11 +499,6 @@ This command does the inverse of `fill-paragraph'."
 (add-hook 'markdown-mode-hook #'rainbow-mode)
 (add-hook 'emacs-lisp-mode-hook #'rainbow-mode)
 
-;; === Avy ===
-
-;; Jump to anything in the buffer
-(rc/require 'avy)
-
 ;; === Keybindings ===
 
 (global-set-key (kbd "C-c M-q") 'rc/unfill-paragraph)
@@ -625,8 +542,6 @@ This command does the inverse of `fill-paragraph'."
 (global-set-key (kbd "H-z c") #'my/zoxide-cd)
 
 (global-set-key (kbd "C-c M-q") 'rc/unfill-paragraph)
-
-(global-set-key (kbd "C-:") 'avy-goto-word-0)
 
 (global-set-key (kbd "S-M-<backspace>") 'backward-mark-word)
 
